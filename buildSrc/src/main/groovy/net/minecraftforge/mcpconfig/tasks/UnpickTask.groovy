4608edf29a974df1ea9afd35d7cf4c2a106616c8
coehlrich <coehlrich@users.noreply.github.com>
2023-12-11 16:50:57 +1300

    Add unpick step

---
+++ buildSrc/src/main/groovy/net/minecraftforge/mcpconfig/tasks/UnpickTask.groovy
@@ -0,0 +1,28 @@
+package net.minecraftforge.mcpconfig.tasks;
+
+import org.gradle.api.*
+import org.gradle.api.file.*
+import org.gradle.api.tasks.*
+
+abstract class UnpickTask extends ToolJarExec {
+    @InputFile abstract RegularFileProperty getUnpick()
+    @InputFile abstract RegularFileProperty getInput()
+    @OutputFile abstract RegularFileProperty getDest()
+    @OutputFile @Optional abstract RegularFileProperty getLog()
+
+    UnpickTask() {
+        mainClass.set "daomephsta.unpick.cli.Main"
+    }
+    
+    @Override
+    protected void preExec() {
+        def logStream = log.isPresent() ? log.get().getAsFile().newOutputStream() : JarExec.NULL_OUTPUT
+        standardOutput logStream
+        errorOutput logStream
+        setArgs(Utils.fillVariables(args, [
+            'unpick': unpick.get().getAsFile().absolutePath,
+            'input': input.get().getAsFile().absolutePath,
+            'output': dest.get().getAsFile().absolutePath
+        ]))
+    }
+}
