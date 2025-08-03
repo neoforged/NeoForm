94b457f2717c35ead4854ec30afcb4633e844677
coehlrich <coehlrich@users.noreply.github.com>
2025-08-03 23:51:37 +1200

    Update gradle to 9

--- buildSrc/src/main/groovy/net/minecraftforge/mcpconfig/tasks/RemapJar.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/mcpconfig/tasks/RemapJar.groovy
@@ -24,8 +24,8 @@ abstract class RemapJar extends ToolJarExec {
     @Override
     protected void preExec() {
         def logStream = log.isPresent() ? log.get().getAsFile().newOutputStream() : JarExec.NULL_OUTPUT
-        standardOutput logStream
-        errorOutput logStream
+        standardOutput = logStream
+        errorOutput = logStream
         setArgs(Utils.fillVariables(args, [
             'mappings': mappings.get().getAsFile().absolutePath,
             'input': input.get().getAsFile().absolutePath,
