264db30c9f209c56559abc86d3746b210e09a69c
coehlrich <coehlrich@users.noreply.github.com>
2025-11-06 02:29:58 +1300

    Add support for unobfuscated versions

--- buildSrc/src/main/groovy/net/minecraftforge/mcpconfig/tasks/SplitJar.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/mcpconfig/tasks/SplitJar.groovy
@@ -9,7 +9,6 @@ import java.util.zip.*
 import net.neoforged.srgutils.IMappingFile
 
 public abstract class SplitJar extends DefaultTask {
-    @InputFile abstract RegularFileProperty getMappings()
     @InputFile abstract RegularFileProperty getSource()
     @OutputFile abstract RegularFileProperty getSlim()
     @OutputFile abstract RegularFileProperty getExtra()
@@ -17,13 +16,12 @@ public abstract class SplitJar extends DefaultTask {
     @TaskAction
     def exec() {
         Utils.init()
-        def classes = IMappingFile.load(mappings.get().getAsFile()).classes.collect{ it.original + '.class' }.toSet()
         
         new ZipOutputStream(slim.get().getAsFile().newOutputStream()).withCloseable{ so ->
             new ZipOutputStream(extra.get().getAsFile().newOutputStream()).withCloseable{ eo ->
                 new ZipInputStream(source.get().getAsFile().newInputStream()).withCloseable{ jin ->
                     for (def entry = jin.nextEntry; entry != null; entry = jin.nextEntry) {
-                        def out = classes.contains(entry.name) ? so : eo
+                        def out = entry.name.endsWith(".class") ? so : eo
                         def oentry = new ZipEntry(entry.name)
                         oentry.lastModifiedTime = entry.lastModifiedTime
                         out.putNextEntry(oentry)
